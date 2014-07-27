0b1ba9d3ee95996aefd54b1157e04eed4c71529a
Chris Elwood <kudzooman@gmail.com>
2014-07-27 10:12:56 -0700

    embedded videos

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index'
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
